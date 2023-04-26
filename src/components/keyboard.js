e41327f66ce9052e98d1ff33f021f0713551a7ad
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-26 20:43:29 +0300

    fix:css style

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -1,9 +1,16 @@
 function creacteKeyBoard() {
     const keyboard = document.createElement('div');
-    keyboard.classList.add('keyboard');
-
+    keyboard.classList.add('keyboard'); 
+    
     return keyboard
 }
 
 
 export default creacteKeyBoard
+
+const keyObj = {
+    eng:{
+        row1:['`' , 1,2,3,4,5,6,7,8,9,0,'-', 'Backspace']
+    },
+    
+}
