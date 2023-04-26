d474951f3b932e68e904e97529e082eb17d4ddff
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-26 21:02:38 +0300

    feat:first row keys eng

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -1,8 +1,27 @@
 function creacteKeyBoard() {
     const keyboard = document.createElement('div');
     keyboard.classList.add('keyboard'); 
-    
+    keyboard.insertAdjacentElement('afterbegin' , createKeys (keyObj))
     return keyboard
+}
+
+function createKeys (obj){
+    const board = document.createElement('div')
+    board.classList.add('keyboard__board')
+    for(let key in  obj.eng) {
+        // console.log(obj.eng)
+        const keyboardRow = document.createElement('div');
+        keyboardRow.classList.add('keyboard__row')
+        obj.eng[key].reverse().forEach(element => {
+            console.log(element)
+            const btnKey = document.createElement('div')
+            btnKey.classList.add('keyboard__key')
+            btnKey.textContent = element;
+            btnKey.setAttribute('data' , element);
+            keyboardRow.insertAdjacentElement('afterbegin' , btnKey)
+        });
+        return keyboardRow
+    }
 }
 
 
@@ -12,5 +31,7 @@ const keyObj = {
     eng:{
         row1:['`' , 1,2,3,4,5,6,7,8,9,0,'-', 'Backspace']
     },
-    
+
 }
+
+
